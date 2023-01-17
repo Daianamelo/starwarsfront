8402d3577a0cb1ee47f9a07bb291948c19ecc013
Daianamelo <melocecilia.daiana@gmail.com>
2023-01-17 18:16:25 +0000

    My first commit

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React,{useEffect,useState} from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
+import Card from "../component/card.js";
 
-export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
-);
+export const Home = () => {
+
+const [personajes, setPersonajes] = useState([])
+
+//hacer una funcion para guardar el fetch
+
+function sacarInfoPersonajes() {
+	fetch("https://www.swapi.tech/api/people")
+	.then(res => res.json())
+	.then(data => data.setPersonajes(data.results))
+	.catch(err =>console.error(err))
+	
+}
+	useEffect (()=>{sacarInfoPersonajes();},[])
+	console.log(personajes);
+
+	// array.map()
+	console.log(personajes);
+
+	//todo lo que se ve se coloca en return
+	return(
+	<>
+	
+	</>
+);};
