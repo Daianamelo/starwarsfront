ab2d025b4747a2459b76e02621e64c60a73879c5
Daianamelo <melocecilia.daiana@gmail.com>
2023-01-18 20:25:34 +0000

    My second commit

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -4,10 +4,10 @@ const Card = (props) => {
     console.log(props);
       return ( 
         <div className="card" style={{width:"18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={props.url} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <h5 className="card-title">{props.nombre}</h5>
+          <p className="card-text">{props.descripcion}</p> 
           <a href="#" className="btn btn-warning">Go somewhere</a>
           <a href="#" className="btn btn-light border-warning"><i className="fa-solid fa-heart"></i></a>
         </div>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -4,27 +4,33 @@ import "../../styles/home.css";
 import Card from "../component/card.js";
 
 export const Home = () => {
+//personajes es el espacio de memoria, setPersonajes es la funcion que actualiza el estado y el estado inicial esta vacio es un array
 
 const [personajes, setPersonajes] = useState([])
+console.log(personajes)
 
 //hacer una funcion para guardar el fetch
 
 function sacarInfoPersonajes() {
 	fetch("https://www.swapi.tech/api/people")
 	.then(res => res.json())
-	.then(data => data.setPersonajes(data.results))
+	// .then(data => data.setPersonajes(data.results))
+	// .then(data =>console.log(data.results)) //me trae el array
+	.then(data =>setPersonajes(data.results))//me setea el array
 	.catch(err =>console.error(err))
 	
 }
 	useEffect (()=>{sacarInfoPersonajes();},[])
-	console.log(personajes);
+	// console.log(personajes);
 
 	// array.map()
-	console.log(personajes);
+	// console.log(personajes);
 
 	//todo lo que se ve se coloca en return
 	return(
 	<>
-	
+	{/* //este me dibuja la carta con su nombre */}
+	<h5>{personajes.map((props)=><Card nombre={props.name} id={props.uid} key={props.uid}/>)}</h5>
+
 	</>
 );};
